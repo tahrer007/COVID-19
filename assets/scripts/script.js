5670368366806316901e00218b7320264ada391b
tahrer007 <tahrer_Ad@outlook.com>
2021-12-10 19:11:30 +0200

    update 19:11

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,7 +3,7 @@ let ContinentsData = {};
 let globalData = {};
 worldsContinents.forEach((element) => {
   ContinentsData[element] = {
-    countriesArr: [1],
+    countriesArr: [],
     confirmed: 0,
     deaths: 0,
     recovered: 0,
@@ -19,7 +19,6 @@ const GlobalData = async () => {
     document.body.innerHTML = "LOADDING ......⏰⏰⏰⌚";
     const data = await (await fetch(timeLIneURL)).json();
     globalData = data.data[0];
-
   } catch (err) {
     console.log(err);
   }
@@ -27,24 +26,27 @@ const GlobalData = async () => {
 };
 //----------------------fill country covid data / calculate content sum (death/comfirmed case / recovered)
 //countryObj={code,name,confirmed,critical,deaths,recovered,calculations{deathrate,recoveryRate},casesPerMillion}
-const calculateContinentCovidData=(countryCovidData,continent)=>{
-  
-    ContinentsData[continent].confirmed+=countryCovidData.confirmed;
-    ContinentsData[continent].deaths+=countryCovidData.deaths;
-    ContinentsData[continent].recovered+=countryCovidData.recovered;
-  
 
-}
+const calculateContinentCovidData = (countryCovidData, continent) => {
+  ContinentsData[continent].confirmed += countryCovidData.confirmed;
+  ContinentsData[continent].deaths += countryCovidData.deaths;
+  ContinentsData[continent].recovered += countryCovidData.recovered;
+};
 //------------------------- getCountryCovidData -------------------
-const getCountryCovidData = async (CountryCode,isCountrySellected,continent) => {
+const getCountryCovidData = async (
+  CountryCode,
+  isCountrySellected,
+  continent
+) => {
   try {
     const countryDataURL = `https://intense-mesa-62220.herokuapp.com/https://corona-api.com/countries/${CountryCode}`;
     document.body.innerHTML = "LOADDING ......⏰⏰⏰⌚";
     const response = await fetch(countryDataURL);
     const countryData = await response.json();
     //console.log(countryData.data.latest_data);
-    
-    if(!isCountrySellected) calculateContinentCovidData(countryData.data.latest_data,continent)
+
+    if (!isCountrySellected)
+      calculateContinentCovidData(countryData.data.latest_data, continent);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${countryData.status}`);
     }
@@ -55,13 +57,14 @@ const getCountryCovidData = async (CountryCode,isCountrySellected,continent) => 
   }
 };
 //----------------------------------------
-const fillcountriesData = (countriesData,continent) => {
+const fillcountriesData = (countriesData, continent) => {
   let countries = [];
+
   countriesData.forEach((element) => {
     let eachcountry = {};
     eachcountry.name = element.name.common;
     eachcountry.code = element.cca2;
-    getCountryCovidData(eachcountry.code, false,continent);
+    getCountryCovidData(eachcountry.code, false, continent);
     countries.push(eachcountry);
   });
   return countries;
@@ -76,10 +79,8 @@ const getCountriesByContinent = async (continent) => {
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${countriesData.status}`);
     }
-    let tempArr = fillcountriesData(countriesData,continent);
+    let tempArr = fillcountriesData(countriesData, continent);
     ContinentsData[continent].countriesArr.push(tempArr);
-    console.log("ContinentsData");
-    console.log(ContinentsData); 
   } catch (err) {
     console.log(err);
   }
@@ -88,5 +89,6 @@ const getCountriesByContinent = async (continent) => {
 
 //----------call the functions --------------
 //GlobalData();
-getCountriesByContinent("Asia");
-//getCountryCovidData("SA",false,'Asia')
+//getCountriesByContinent("Asia");
+//TODO : get data for all containent 
+console.log(ContinentsData);
