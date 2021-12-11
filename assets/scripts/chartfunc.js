286b22796679de7b42e9e1bee7ae24b08c0d1ca1
tahrer007 <tahrer_Ad@outlook.com>
2021-12-11 14:46:56 +0200

    14:46

---
+++ assets/scripts/chartfunc.js
@@ -0,0 +1,3 @@
+import * as selectors from "./selectors.js"
+import * as data from "./getData.js"
+import * as Utils from "./Utils.js"
--- assets/scripts/globalChart.js
+++ assets/scripts/globalChart.js
@@ -1,44 +1,90 @@
-//setup 
-const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-  ];
-  const data = {
-    labels: labels,
-    datasets: [{
-      label: 'daily new deaths',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
+import * as chartData from "./getData.js"
+//setup
+const labels = ["January", "February", "March", "April", "May", "June"];
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: "daily new deaths",
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "rgb(255, 99, 132)",
       data: [0, 10, 5, 2, 20, 30, 45],
+      pointRadius: 10,
+      pointHoverRadius: 15,
+      pointHoverBackgroundColor: 'blue',
+     
+      
     },
     {
-        label: 'daily new cases',
-        backgroundColor: 'blue',
-        borderColor: 'blue',
-        data: [50, 30,40, 8, 15, 14, 90],
-      }
-
-]
-  };
-//config 
+      label: "daily new cases",
+      backgroundColor: "blue",
+      borderColor: "blue",
+      data: [50, 30, 40, 8, 15, 14, 90],
+      
+    },
+  ],
+};
+//config
 const config = {
-    type: 'line',
-    data: data,
-    options: {
-      plugins: {
+  type: "line",
+  data: data,
+  options: {
+    tooltips: {
+        /*callbacks:{
+
+        }*/
+      },
+    plugins: {
+        legend: {
+            labels: {
+                // This more specific font property overrides the global property
+                font: {
+                    size: 50
+                }
+            }
+        }
+    },
+    responsive: true,
+    scales: {
+      x: {
+        display: true,
         title: {
           display: true,
-          text: 'last 7 days new deaths and cases ',
-        }
+          text: "world last seven days deaths and new cases ",
+          color: "#911",
+          font: {
+            family: "Comic Sans MS",
+            size: 50,
+            weight: "bold",
+            lineHeight: 1.2,
+          },
+          padding: { top: 20, left: 0, right: 0, bottom: 0 },
+        },
+
+        ticks: {
+            // For a category axis, the val is the index so the lookup via getLabelForValue is needed
+            color: 'red',
+            font: {
+                size:40
+            }
+          }  
+      },
+      y:{
+        ticks: {
+            // For a category axis, the val is the index so the lookup via getLabelForValue is needed
+            color: 'red',
+            font: {
+                size:40
+            },
+            
+          },
+          min: 0,
+          max:50
+         
+          
       }
-    }
-  };
-//start 
-const myChart = new Chart(
-    document.getElementById('myChart'),
-    config
-  );
+    },
+  },
+};
+//start
+const myChart = new Chart(document.getElementById("myChart"), config);
